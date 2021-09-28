c28c00f09706e6c1d78bb814abc71c7bca09dfb2
Dramane <Dramane.dev@gmail.com>
2021-09-28 16:00:09 +0200

    Back modified

--- functions/getGeneralScore.js
+++ functions/getGeneralScore.js
@@ -1,15 +1,8 @@
 async function getGeneralScore(sequelize, username) {
     return await new Promise((resolve, reject) => {
-        let generalScore = 0;
-        // SELECT `username`, AVG(score) FROM `messages` GROUP BY `username`
-        sequelize.query("SELECT * FROM `messages` WHERE username='" + username + "'").then(([results]) => {
-            console.log('username : ' + username)
-            console.log(results)
-            results.map(value => {
-                generalScore += value.score
-            });
-            console.log('General score is ' + generalScore);
-            return resolve(generalScore);
+        sequelize.query("SELECT `username`, AVG(score) AS score FROM `messages` GROUP BY `username`").then(([results]) => {
+            console.log(results);
+            return resolve(results.find((us) => us.username === username).score);
         }).catch(err => {
             reject(err.message);
         });
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 const { PythonShell } = require('python-shell');
 const { getGeneralScore } = require('./functions/getGeneralScore');
+const fs = require('fs');
+const xlsx = require('xlsx');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
@@ -23,7 +25,7 @@ const port = process.env.SERVER_PORT || 3000;
 const sequelize = new Sequelize(
     process.env.DB_NAME || "chatcloud",
     process.env.DB_USER || "root",
-    process.env.DB_PWD || "rootroot",
+    process.env.DB_PWD || "root",
     {
         dialect: "mysql",
         host: process.env.DB_HOST || "localhost",
@@ -33,7 +35,7 @@ const sequelize = new Sequelize(
 );
 try {
     sequelize.authenticate();
-    sequelize.query('SELECT * FROM loggedUsers').then(() => {});
+    sequelize.query('SELECT * FROM users').then(() => {});
     console.log('Connecté à la base de données MySQL!');
 } catch (error) {
     console.error('Impossible de se connecter, erreur suivante :', error);
@@ -47,19 +49,46 @@ server.listen(port, () => {
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-const loggedUsers = [];
+const generateXlsx = async () => {
+    let users = [];
+    return new Promise((resolve, reject) => {
+        let userObject = {};
+
+        sequelize.query("SELECT * FROM `users`").then(([result]) => {
+            result.map(user => {
+                userObject.id = user.id;
+                userObject.username = user.username;
+                userObject.score = user.score;
+                users.push(userObject);
+                userObject = {};
+            });
+            fs.writeFileSync('./data/data.json', JSON.stringify(users));
+            return resolve('json generated');
+        }).catch(err => {
+            if (err) reject(err.message);
+        });
+    });
+}
+
+app.get('/generate-xlsx', (req, res) => {
+    generateXlsx();
+});
+
+
+
+let loggedUsers = [];
 
 //io.adapter(redis({ host: process.env.REDIS_HOST || 'localhost', port: process.env.REDIS_PORT || 6379 }));
 io.on('connection', (socket) => {
     let addedUser = false;
-    console.log(loggedUsers);
     // when the client emits 'new message', this listens and executes
     socket.on('new message', (data) => {
-        //data = data.includes("'") ? data.replace(/[']/g, "\'") : data;
         data = data.includes("'") ? data.replace("'", " ") : data;
 
-        sequelize.query("INSERT INTO `messages`(`id`, `message`, `username`) VALUES (null, '" + data + "', '" + socket.username + "')").then(() => {
+        console.log(socket.username, 'send message')
+        sequelize.query("INSERT INTO `messages`(`id`, `message`, `username`, `score`) VALUES (null, '" + data + "', '" + socket.username + "', 0)").then((message) => {
             // we tell the client to execute 'new message'
+            const newMessageId = message[0];
             let score = 0;
 
             const runPython = async (data, sequelize) => {
@@ -70,26 +99,18 @@ io.on('connection', (socket) => {
                 const result = await new Promise((resolve, reject) => {
                     PythonShell.run('./scripts/toneAnalyzer.py', options, (err, results) => {
                         if (err) reject(err.message);
-
-                        console.log(results);
-
                         if (JSON.parse(results[0]).document_tone.tones[0] !== undefined) {
                             let score = JSON.parse(results[0]).document_tone.tones[0].score;
                             let toneName = JSON.parse(results[0]).document_tone.tones[0].tone_name;
-            
-                            console.log('Score ' + score);
-                            console.log('Tone name ' + toneName);
                             return resolve(score);
                         }
-                        reject('Impossible to get score');
+                        resolve(0);
                     });
                 });
                 score = result;
 
-                await sequelize.query("UPDATE `messages` SET `score` = " + score + " WHERE `message` = '" + data + "'")
-                 .then(() => {
-                     console.log('score saved into messages');
-                 })
+                await sequelize.query("UPDATE `messages` SET `score` = " + score + " WHERE `id` = '" + newMessageId + "'")
+                 .then(() => {})
                  .catch(err => {
                      if (err) console.error(err.message);
                  }) 
@@ -98,17 +119,28 @@ io.on('connection', (socket) => {
 
             try {
                 (async () => {
-                    let score = await runPython(data, sequelize);  
-                    console.log('Score ' + score);
-                    socket.broadcast.emit('new message', {
+                    let score = await runPython(data, sequelize);
+                    const newMessage = {
                         username: socket.username,
                         message: data,
                         score: score
+                    };
+                    console.log('Score ' + score, newMessage);
+                    socket.broadcast.emit('new message', newMessage);
+                    socket.emit('new message', newMessage);
+                    // Fonction calcul score global utilisateur
+                    let generalScore = await getGeneralScore(sequelize, socket.username);
+                    socket.broadcast.emit('update user score', {
+                        username: socket.username, 
+                        score: generalScore
                     });
-                    socket.emit('new message', {
-                        username: socket.username,
-                        message: data,
-                        score: score
+                    socket.emit('update user score', {
+                        username: socket.username, 
+                        score: generalScore
+                    });
+                    //  TRIGGER UN MAIL
+                    await sequelize.query("UPDATE `users` SET `score` = " + generalScore + "WHERE `username`='" + socket.username + "'").then(() => {}).catch(err => {
+                        if (err) console.log(err.message);
                     });
                 })();
             } catch (error) {
@@ -124,48 +156,29 @@ io.on('connection', (socket) => {
         socket.username = username;
         addedUser = true;
 
-        sequelize.query("SELECT * FROM `loggedusers` WHERE `username`='" + socket.username + "'").then(([result]) => {
-            if (result.length > 0) {
-                result.map(user => {
-                    loggedUsers.push({
-                        id: user.id,
-                        username: user.username
-                    });
-                });
-                socket.emit('login', result.map((r) => r.username));
-                // echo globally (all clients) that a person has connected
-                socket.broadcast.emit('user joined', socket.username);
-                
-                sequelize.query('SELECT * FROM messages LIMIT 50').then(([result]) => {
-                    socket.emit('previous messages', result.map((r) => ({ username: r.username, message: r.message })));
-                });
-            } else {
-                // we store the username in the socket session for this client
-                sequelize.query("INSERT INTO `loggedUsers` (`id`, `username`) VALUES (null, '" + username + "')").then(() => {
-                    sequelize.query('SELECT * FROM `loggedUsers`').then(([result]) => {
-                        socket.emit('login', result.map((r) => r.username));
-                        // echo globally (all clients) that a person has connected
-                        socket.broadcast.emit('user joined', socket.username);
-                        result.map(user => {
-                            loggedUsers.push({
-                                id: user.id,
-                                username: user.username
-                            });
-                        })
-                    })
-                    .then(() => {
-                        console.log('User saved in Database');
-                    })
-                    .catch(err => {
-                        if (err) console.log(err.message);
-                    });
-                    
+        sequelize.query("SELECT * FROM `users` WHERE `username`='" + socket.username + "'").then(([result]) => {
+            if (result.length === 0) {
+                sequelize.query("INSERT INTO `users` (`id`, `username`, `score`) VALUES (null, '" + username + "', 0)").then((user) => {
+                    const newUserId = user[0];
+                    const newUser = {id: newUserId, username, score: 0};
+                    loggedUsers.push(newUser);
+                    socket.emit('login', loggedUsers);
+                    socket.broadcast.emit('user joined', newUser);
                     sequelize.query('SELECT * FROM messages LIMIT 50').then(([result]) => {
-                        socket.emit('previous messages', result.map((r) => ({ username: r.username, message: r.message })));
+                        socket.emit('previous messages', result.map((r) => ({username: r.username, message: r.message, score: r.score})));
                     });
                 })
                 .catch(err => {
                     if (err) console.log(err.message);
+                });
+            } else {
+                const user = result[0];
+                const newUser = {id: user.id, username: user.username, score: user.score};
+                loggedUsers.push(newUser);
+                socket.emit('login', loggedUsers);
+                socket.broadcast.emit('user joined', newUser);
+                sequelize.query('SELECT * FROM messages LIMIT 50').then(([result]) => {
+                    socket.emit('previous messages', result.map((r) => ({username: r.username, message: r.message, score: r.score})));
                 });
             }
         });
@@ -184,30 +197,9 @@ io.on('connection', (socket) => {
     // when the user disconnects.. perform this
     socket.on('disconnect', () => {
         if (addedUser) {
-            loggedUsers.filter(user => user.name !== socket.username);
+            loggedUsers = loggedUsers.filter(user => user.username !== socket.username);
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', socket.username);
         }
-        loggedUsers.filter(user => user.name !== socket.username);
     });
-
-    setInterval(async () => {
-        let generalScore = await getGeneralScore(sequelize, socket.username);
-        console.log('General score ' + generalScore);
-
-        if (socket.username !== undefined) {
-            await sequelize.query("UPDATE `loggedUsers` SET `score` = " + generalScore + "WHERE `username`='" + socket.username + "'").then(() => {
-                console.log('General score saved into databse');
-            }).catch(err => {
-                if (err) console.log(err.message);
-            });
-        }
-
-        socket.broadcast.emit('general score', {
-            username: socket.username,
-            generalScore: generalScore
-        });
-        socket.emit('general score', {
-            username: socket.username,
-            generalScore: generalScore
-        });
-    }, 60000);
 });
